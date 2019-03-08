44398fc1021b2633715473485c973a8bb8c806a8
hill10sheridan <hill10@sheridancollege.ca>
2019-03-08 12:35:53 -0500

    Added alt path and check

--- src/midtermexam_versiona_extensioncode/TestUserprofile.java
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -6,8 +6,9 @@ Date:
 
 Description:
 
-*/
+ */
 package midtermexam_versiona_extensioncode;
+
 import java.util.Scanner;
 
 /**
@@ -15,18 +16,27 @@ import java.util.Scanner;
  * @author Ryan Hill
  */
 public class TestUserprofile {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String inName = s.nextLine();
+        System.out.println("Choose your favourite genre");
+        System.out.println("Comedy, Drama, Action, Mystery");
+        String inGenre = s.nextLine();
+
+        if (inGenre.equalsIgnoreCase("Comedy") 
+                || inGenre.equalsIgnoreCase("Action")
+                || inGenre.equalsIgnoreCase("Drama")
+                || inGenre.equalsIgnoreCase("Mystery")) {
+            UserProfile user = new UserProfile(inName, inGenre);
+            System.out.println("Your userProfile was created");
+        } else {
+        System.out.println("UserProfile creation failed");
+        }
         
-    Scanner s = new Scanner(System.in);
-    System.out.println("Enter your name");
-    String inName = s.nextLine();
-    System.out.println("Choose your favourite genre");
-    System.out.println("Comedy, Drama, Action, Mystery");
-    String inGenre = s.nextLine();
-    
-    UserProfile user = new UserProfile(inName, inGenre);
-    System.out.println("Your userProfile was created");
-    
+
     }
 
 }
